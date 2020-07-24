bd330c6d5a1df62d6d95a73028db8a8069bdbd50
realyxf <997915421@qq.com>
2020-07-24 22:48:47 +0800

    [Parking Lot][Fennie] fearture: pass should_return_null_when_parking_given_ticket_with_more_than_capacity.

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -7,12 +7,12 @@ public class ParkingBoy {
     Set<String> parkLotSet = new HashSet<>();
 
     public Ticket parking(String carId) {
-//        if (parkLotSet.size() < 10) {
-//            int ticketIdNum = (int) (Math.random() * 900) + 100;
-//            Ticket ticket = new Ticket(String.format("T%s", ticketIdNum), carId, false);
-//            parkLotSet.add(ticket.getCarId());
-//            return ticket;
-//        }
+        if (parkLotSet.size() < 10) {
+            int ticketIdNum = (int) (Math.random() * 900) + 100;
+            Ticket ticket = new Ticket(String.format("T%s", ticketIdNum), carId, false);
+            parkLotSet.add(ticket.getCarId());
+            return ticket;
+        }
         return null;
     }
 
