14bc43e1c05065ce4dd8cf060c4b8b33719682aa
realyxf <997915421@qq.com>
2020-07-24 20:55:23 +0800

    [Parking Lot][Fennie] test: should_return_correct_car_when_fetching_given_ticket() and it has passed

--- src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
@@ -33,4 +33,18 @@ class ParkingBoyFacts {
         //then
         assertEquals(carId,result);
     }
+
+    @Test
+    void should_return_correct_car_when_fetching_given_ticket() {
+        //given
+        String ticketId = "T001";
+        String carId = "C001";
+        ParkingBoy parkingBoy = new ParkingBoy();
+
+        //when
+        String result = parkingBoy.fetching(ticketId);
+
+        //then
+        assertEquals(carId,result);
+    }
 }
